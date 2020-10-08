c3fe762ac18308e517ad74474cee8e6a28b4df92
Michael Kim <m.kim4247@gmail.com>
2020-10-08 19:39:43 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(array)
-  counter = 0 
-  while counter < array.length 
-    yield array[counter]
-    counter += 1 
-  end 
-  array 
+  i = 0
+  while i < array.length
+    yield array[i]
+    i+=
+  end
+  array
 end
